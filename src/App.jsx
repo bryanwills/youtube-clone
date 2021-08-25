70746378a75b580959a146328d1ed89ee2811cfc
The Lissimus <thelissimus@tuta.io>
2021-08-25 21:34:47 +0500

    Code splitting and data fetching optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import { VideosContext } from './context/VideosContext';
+import './assets/styles/main.scss';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
-import Feed from './components/Feed/Feed';
-import Channel from './components/Channel/Channel';
-import Video from './components/Video/Video';
-import './assets/styles/main.scss';
+const Feed = lazy(() => import('./components/Feed/Feed'));
+const Channel = lazy(() => import('./components/Channel/Channel'));
+const Video = lazy(() => import('./components/Video/Video'));
 
 function App() {
   const { pathname } = useLocation();
   const [videos, setVideos] = useState([]);
   const fetch_data = {
-    url: 'https://jsonplaceholder.typicode.com/photos',
+    url: 'https://jsonplaceholder.typicode.com/photos?albumId=1',
     limit: 10,
   };
 
@@ -37,9 +37,11 @@ function App() {
 
         <VideosContext.Provider value={{ videos, setVideos }}>
           <Switch>
-            <Route exact path="/" component={Feed} />
-            <Route path="/channel" component={Channel} />
-            <Route path="/video/2" component={Video} />
+            <Suspense fallback={<div />}>
+              <Route exact path="/" component={Feed} />
+              <Route path="/channel" component={Channel} />
+              <Route path="/video/2" component={Video} />
+            </Suspense>
           </Switch>
         </VideosContext.Provider>
       </section>
